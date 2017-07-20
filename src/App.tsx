a1d213c903da2e8052c09cfb28e511106e3a8106
Tom Klaver <tomklav@gmail.com>
2017-07-20 17:53:34 +0200

    add bootstrap, react-jsonschema-form experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
 
 import './App.css';
-import logo from './logo.svg';
 
-export class App extends React.Component<{}, {}> {
+import {Col, Well} from 'react-bootstrap';
+import Form from 'react-jsonschema-form';
+
+export class App extends React.Component<{}, { schema: object, formData: object }> {
+  componentWillMount() {
+    this.setState({});
+    fetch('software.schema.json').then((data) => data.json()).then((data) =>
+      this.setState({ schema : data })
+    );
+  }
+
+  updateFormData = (data: any) => {
+      this.setState({formData: data});
+  }
+
+  form = () => (
+    <Form
+        schema={this.state.schema}
+        onChange={this.updateFormData}
+        formData={this.state.formData}
+    />
+  )
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Col md={8}>
+          {this.state.schema && this.form()}
+        </Col>
+        <Col md={4}>
+          <Well>
+            test
+          </Well>
+        </Col>
       </div>
     );
   }
