7aebadcd939ef46a78d259aefd09b0dfb8205831
Tom Klaver <tomklav@gmail.com>
2017-10-16 14:09:36 +0200

    rm test code

--- scripts/ibuilder/index.js
+++ scripts/ibuilder/index.js
@@ -10,14 +10,14 @@ const regexpExport = /\nexport.*?\n/;
 const BASE_URL = 'https://raw.githubusercontent.com/NLeSC/research-software-directory-backend/master/schema';
 const API_RESOURCE_URL = 'https://api.github.com/repos/NLeSC/research-software-directory-backend/contents/schema';
 const localPath = path.join(__dirname, '..', '..', '..', 'research-software-directory-backend', 'schema');
-const localPathExists = fs.existsSync(localPath) && false;
+const localPathExists = fs.existsSync(localPath);
 const outputPath = path.join(__dirname, '..', '..', 'src', 'interfaces');
 
 let resourceInterface = '';
 
 if (localPathExists) { console.log('reading from local path ' + localPath); }
 
-const jsonRequest = url => console.log(url) || new Promise((resolve, reject) =>
+const jsonRequest = url => new Promise((resolve, reject) =>
     request({
         url: url,
         headers: {'user-agent': 'node/requests'}
