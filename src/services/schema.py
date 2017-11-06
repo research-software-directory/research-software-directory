ff6e014f771ff2d53adea6f07863dbbfb1ddd2d9
Tom Klaver <tomklav@gmail.com>
2017-11-06 17:26:20 +0100

    add initial test for validator

--- src/services/schema.py
+++ src/services/schema.py
@@ -4,6 +4,12 @@ import os
 from jsonschema import validate, ValidationError
 
 
+def add_properties(base_schema, filename):
+    # add additional properties from properties folder
+    properties = json.load(open('schema/properties/' + filename))
+    for prop_name in properties:
+        base_schema['properties'][prop_name].update(properties[prop_name])
+
 class SchemaService:
     def __init__(self, db):
         self.db = db
@@ -11,16 +17,16 @@ class SchemaService:
         for (_, _, files) in os.walk('schema'):
             for (filename, file) in [(filename, open('schema/' + filename)) for filename in files]:
                 base_schema = json.load(file)
-                properties = json.load(open('schema/properties/' + filename))
-                for prop_name in properties:
-                    base_schema['properties'][prop_name].update(properties[prop_name])
+                add_properties(base_schema, filename)
                 self.schema[filename.split('.')[0]] = base_schema
 
     def verify_data(self, fix=True):
+        valid = True
         for software in self.db.software.all():
             try:
                 validate(software.data, self.schema['software'])
             except ValidationError as e:
+                valid = False
                 if fix and str(e.message) == "None is not of type 'array'":
                     print('fixing None to []')
                     software[e.path[0]] = []
@@ -29,3 +35,4 @@ class SchemaService:
                 print('in %s: %s' % (software['id'], ' -> '.join(str(path_elm) for path_elm in e.path)))
                 print(e.message)
                 print('\n')
+        return valid
--- tests/unit/services/test_schema.py
+++ tests/unit/services/test_schema.py
@@ -1,6 +1,8 @@
 from src.database.database_dict import DictDatabase
 from src.services import schema
 import pytest
+
+
 @pytest.fixture(autouse=True)
 def db():
     return DictDatabase({
@@ -12,12 +14,19 @@ def db():
         ]
     })
 
+
 @pytest.fixture(autouse=True)
 def service(db):
     return schema.SchemaService(db)
 
+
 def test_service_exists(service):
     assert service is not None
 
+
 def test_has_extra_properties(service):
     assert service.schema['software']['properties']['createdAt']['editable'] is False
+
+
+def test_validate_data(service):
+    assert service.verify_data(False) is False
