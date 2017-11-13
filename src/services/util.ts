9897ac1b266336e65460f7c9a9fc4cb34b7ab17e
Tom Klaver <tomklav@gmail.com>
2017-11-13 17:10:39 +0100

    better name splitting

--- src/services/util.ts
+++ src/services/util.ts
@@ -1,5 +1,11 @@
 export const splitNames = (name: string) => {
   /* splits name into first & last name, returns string[2]; */
+
+  if (name.indexOf(',') !== -1) {
+    const parts = name.split(',');
+    return [parts[1], parts[0]];
+  }
+
   const nameParts = name.split(' ');
   return [nameParts[0], nameParts[nameParts.length - 1]];
 };
