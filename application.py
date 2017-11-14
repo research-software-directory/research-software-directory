1e1c5aea8f87c0cb822e35f5c44ffd3210c73bac
Laurens Bogaardt <lbogaardt@gmail.com>
2017-11-14 09:43:00 +0100

    added name to list in index

--- application.py
+++ application.py
@@ -8,8 +8,7 @@ app = flask.Flask(__name__)
 def index():
     url = "http://admin.research-software.nl/api/software"
     all_software_dictionary = requests.get(url).json()
-    id_list = [software_data["id"] for software_data in all_software_dictionary]
-    return flask.render_template('index_template.html', template_data = id_list)
+    return flask.render_template('index_template.html', template_data = all_software_dictionary)
 
 @app.route('/software/<software_id>')
 def software_product_page_template(software_id):
