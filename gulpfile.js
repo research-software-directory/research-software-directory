729435d380facbadf9ed4c6f77c12d4385a8fbb5
Tom Klaver <tomklav@gmail.com>
2017-12-13 18:32:00 +0100

    clean index, add date formatter, scss source maps

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('watch', ['build'], function(gulpCallback) {
     gulp.watch('/style/**/*.scss', ['build']);
@@ -15,7 +16,9 @@ gulp.task('build', function() {
     ];
 
     return gulp.src('/style/**/*.scss')
-        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss(plugins))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('/static/style'))
 });
