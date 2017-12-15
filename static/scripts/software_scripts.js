92ff088e54180d16c85940e6102894211758a2a6
Tom Klaver <tomklav@gmail.com>
2017-12-15 14:17:19 +0100

    move plot_commits js to software_scripts

--- static/scripts/software_scripts.js
+++ static/scripts/software_scripts.js
@@ -42,3 +42,41 @@ $(document).ready(function() {
         return false;
     });
 });
+
+
+
+
+$(document).ready(function() {
+
+    function plot_commits(data) {
+        var plotid = document.getElementById("commitsPlot");
+        var layout = {
+            autosize: true,
+            margin: {l:20,r:50,b:40,t:20},
+            xaxis: {
+                type: 'date',
+                autotick: false,
+                ticks: 'outside',
+                tick0: '2000-01-15',
+                dtick: 'M12',
+                tickformat: "%Y"
+            },
+            paper_bgcolor: "rgba(0,0,0,0)",
+            plot_bgcolor: "rgba(0,0,0,0)"
+        };
+        Plotly.newPlot(plotid, data, layout, {displayModeBar: false, staticPlot: true});
+        var update = {
+            "marker.color": "rgb(100,100,100)"
+        };
+        Plotly.restyle(plotid, update);
+    }
+
+    var statid = document.getElementById("commitsStat");
+    if ('error' in commitsData) {
+        statid.innerHTML = commitsData['error'];
+    } else {
+        plot_commits(commitsData['plot']);
+        statid.innerHTML = '<b>' + commitsData['total'] + '</b> commits | Last update: <b>' + commitsData['last'] + '</b>';
+    }
+
+});
