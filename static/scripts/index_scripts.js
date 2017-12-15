1eed32bcb63d72bfe849af973cec2862f98ee69f
Tom Klaver <tomklav@gmail.com>
2017-12-15 13:57:44 +0100

    move overview script to .js file

--- static/scripts/index_scripts.js
+++ static/scripts/index_scripts.js
@@ -1,137 +1,134 @@
-function search_algo(search_key, stype){
-  search_key = search_key.toLowerCase();
-  count = 0;
-  search_key_split = search_key.trim().split(" ");
-  console.log(search_key_split)
-    for (var i = 0; i <ALL_DATA.length; i++){
-      dict = ALL_DATA[i];
-      hit = false;
-      hit_count = 0
-        for (var j =0; j < dict['discipline'].length; j++){
-          term = dict['discipline'][j]
-          for (var idx in search_key_split){
-            if (term.toLowerCase().indexOf(search_key_split[idx]) !== -1){
-                hit_count += 1
-                break;
-            }
-         }
-        }
-        for (var j = 0; j < dict['expertise'].length; j++){
-          term = dict['expertise'][j]
-          for (var idx in search_key_split){
-            if (term.toLowerCase().indexOf(search_key_split[idx]) !== -1){
-                hit_count += 1
-                break;
-            }
-          }
-        }
-        for (var j = 0; j < dict['tags'].length; j++){
-          term = dict['tags'][j]
-          for (var idx in search_key_split){
-            if (term.toLowerCase().indexOf(search_key_split[idx]) !== -1){
-                hit_count += 1
-                break;
-            }
-          }
-        }
-        for (var idx in search_key_split){
-          if (dict['tagLine'].toLowerCase().indexOf(search_key_split[idx])!==-1){
-            hit=true
-            hit_count += 1
-          }
-        }
-        if(hit_count>0){
-            $("#"+ALL_DATA[i]['id']+".product").css("display", "block")
-          count += 1
-        }else if(stype=="filter"){
-          $("#"+ALL_DATA[i]['id']+".product").css("display", "none")
-        }
+/*** Copyright 2013 Teun Duynstee Licensed under the Apache License, Version 2.0 ***/
+var firstBy=function(){function n(n){return n}function t(n){return"string"==typeof n?n.toLowerCase():n}function r(r,e){if(e="number"==typeof e?{direction:e}:e||{},"function"!=typeof r){var i=r;r=function(n){return n[i]?n[i]:""}}if(1===r.length){var u=r,o=e.ignoreCase?t:n;r=function(n,t){return o(u(n))<o(u(t))?-1:o(u(n))>o(u(t))?1:0}}return-1===e.direction?function(n,t){return-r(n,t)}:r}function e(n,t){var i="function"==typeof this&&this,u=r(n,t),o=i?function(n,t){return i(n,t)||u(n,t)}:u;return o.thenBy=e,o}return e}();
+Vue.config.devtools = true;
 
-  }
-  results_list = "There are "+count+" search results for the keyword <b>"+search_key+"</b>"
-  return results_list
-}
+function initOverview(softwareData) {
+    var v = new Vue({
+        el: '#overview',
+        delimiters: ["[[", "]]"],
+        methods: {
+            log: console.log,
+            showPage: function (n) {
+                return n === 1 || n === this.lastPage || Math.abs(n - this.page) <= 2
+            }
+        },
+        data: {
+            tags: [
+                "Visualization",
+                "Image processing",
+                "Machine learning",
+                "Text analysis & natural language processing",
+                "Real time data analysis",
+                "Optimized data handling",
+                "Big data",
+                "Inter-operability & linked data",
+                "Multi-scale & multi model simulations",
+                "High performance computing",
+                "GPU",
+                "Workflow technologies"
+            ],
+            filter: {
+                search: '',
+                tags: []
+            },
+            sort: 'Last updated',
+            pageSize: 10,
+            page: 1,
+            software: softwareData
+        },
+        computed: {
+            tagCount: function () {
+                // initialize to 0
+                var counts = this.tags.reduce(function (acc, cur) {
+                    acc[cur] = 0;
+                    return acc;
+                }, {});
+                this.software.forEach(function (sw) {
+                    sw.tags.forEach(function (tag) {
+                        counts[tag] += 1;
+                    });
+                });
+                return counts;
+            },
 
-=
+            filteredSoftware: function () {
+                function filterTags(tags) {
+                    return function (sw) {
+                        if (tags.length === 0) return true;
+                        var match = false;
+                        sw.tags.forEach(function (tag) {
+                            if (tags.includes(tag)) {
+                                match = true;
+                            }
+                        });
+                        return match;
+                    }
+                }
 
- $(document).ready(function(){
-    // fill gems div
-    default_number_all_projects = 200
-    project_count = 0
-    for (var i = 0; i < ALL_DATA.length; i++){
-      var toolDate = new Date()
-      toolDate.setSeconds(parseInt(ALL_DATA[i]['lastUpdate']))
-      var formattedDate = toolDate.toDateString();
-      var toolTipText = '<b>'+ALL_DATA[i]['name']+'</b><br><br>'+ALL_DATA[i]['tagLine']+'<br><br>Commits : '+ALL_DATA[i]['commits']+'<br> Mentions : '+ALL_DATA[i]['mentions'] +'<br><br>Last updated:'+formattedDate;
-      var appendHtml = "<div class='product' id="+ALL_DATA[i]['id']+"><a href='/software/"+ALL_DATA[i]['id']+"'><b>"+ALL_DATA[i]['id']+"</b><img class='product_thumb' data-placement='left' data-toggle='tooltip' data-html='true' title='" + toolTipText + "' src='static/images/thumb_default.jpg'></img></a></div>";
-      if (ALL_DATA[i]['highlighted']) {
-        $("#data_gems").append(appendHtml) // name, tagline, mentions, commits
-      }
-      if(project_count<default_number_all_projects){
-        $("#data_display").append(appendHtml) // name, tagline, mentions, commits
-      project_count += 1
-      }
-    }
-    // a
-    // activate Bootstrap4 on the tooltips
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip();
+                function filterSearch(searchTerm) {
+                    return function (sw) {
+                        if (!searchTerm) return true;
+                        var fields = sw.name + " " + sw.tagLine + ' ' + sw.tags.join(" ");
+                        return fields.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+                    }
+                }
+
+                return this.software
+                    .filter(filterTags(this.filter.tags))
+                    .filter(filterSearch(this.filter.search));
+            },
+
+            sortedSoftware: function () {
+                function updatedSorter(a, b) {
+                    return b.lastUpdate - a.lastUpdate;
+                }
+
+                function promoteHighlighted(a, b) {
+                    if (a.highlighted && b.highlighted) return 0;
+                    else if (a.highlighted) return -1;
+                    else if (b.highlighted) return 1;
+                    return 0;
+                }
+
+                function sortFunction(sortVal) {
+                    switch (sortVal) {
+                        case 'Last updated':
+                            return updatedSorter;
+                        default:
+                            return updatedSorter;
+                    }
+                }
+
+                if (this.sort === 'Last updated' && !this.filter.search && this.filter.tags.length === 0) {
+                    return this.filteredSoftware.sort(firstBy(promoteHighlighted).thenBy(updatedSorter));
+                } else {
+                    return this.filteredSoftware.sort(sortFunction(this.sorts));
+                }
+            },
+
+            pagedSoftware: function () {
+                var offset = this.pageSize * (this.page - 1);
+                return this.sortedSoftware.slice(offset, offset + this.pageSize);
+            },
+
+            lastPage: function () {
+                return Math.ceil(this.filteredSoftware.length / this.pageSize);
+            },
+
+            searchFields: function () {
+                this.software.map(function (sw) {
+                    return "test";
+                });
+            }
+        },
+
+        watch: {
+            sort: {
+                handler: function () { this.page = 1; } },
+            filter: {
+                handler: function () { this.page = 1; },
+                deep: true
+            }
+        }
     });
- });
-
-  $("form input[type=submit]").click(function() {
-      $("input[type=submit]", $(this).parents("form")).removeAttr("clicked");
-      $(this).attr("clicked", "true");
-  });
-
- $("#search").on("submit", function (event){
-    event.preventDefault();
-    console.log($("#reset").attr("clicked"))
-    if( $("input[type=submit][clicked=true]").val()=='search'){
-      $("*.product").css("display", "none")
-      $("#search_results.layout").empty()
-      results_list = search_algo(search.input.value, "search")
-
-      $("#box_1").css("display", "none")
-      /*$("#box_0").css("display", "block")*/
-      $('input:checkbox:checked').prop('checked', false);
-      $("#search_results.layout").append(results_list)
-      $("#search_results.layout").css("display", "block")
-    }else if( $("input[type=submit][clicked=true]").val()==='reset'){
-      $("#box_1").css("display", "block")
-      $("#search_results.layout").empty()
-      $("#search_results.layout").css("display", "none")
-      $("*.product").css("display", "block")
-    }
-  });
-
-$("#filter").click(function (event){
-  //event.preventDefault(); 
-  $("#box_1").css("display", "none")
-  console.log("Filter selection:")
-  var search_key = ""
-  var highlights_selected = false
-  $('input[type=checkbox').each(function(){
-      //console.log($(this).val()+" checked:"+$(this).prop('checked'))
-      if ($(this).prop('checked')){              
-        if($(this).val()=='highlights'){
-          $("*.product").css("display", "block")
-          $('#box_0').css("display", "none")
-          $('#box_1').css("display", "block")
-          highlights_selected = true
-        }
-      search_key += $(this).val()+" "
-      }
-  })
-  $("#search_results.layout").empty()
-  $("#search_results.layout").css("display", "none")
-  console.log(search_key)
-  if (highlights_selected==false){
-    $("#box_1").css("display", "none")
-  }
-  $("#search_results.layout").empty()
-  results_list = search_algo(search_key, "filter")
-  $("#search_results.layout").append(results_list)
-  $("#search_results.layout").css("display", "block")
-});
-
+}
