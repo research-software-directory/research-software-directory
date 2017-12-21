974f76907187b5289e84b3fcb2d37bb3c350130c
Tom Klaver <tomklav@gmail.com>
2017-12-21 14:38:41 +0100

    add ENVIRONMENT var

--- src/settings.py
+++ src/settings.py
@@ -12,28 +12,30 @@ required_settings = [
     "DATABASE_HOST",
     "DATABASE_PORT",
     "DATABASE_NAME",
-    "CELERY_BROKER_URL"
+    "CELERY_BROKER_URL",
+    "CELERY_BACKEND_URL",
+    "ENVIRONMENT"
 ]
 
 for setting in required_settings:
     settings[setting] = None
 
-try:
-    import os
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    settings_file_name = os.path.join(this_dir, '..', 'settings.json')
-    with open(settings_file_name) as settings_file:
-        data = json.load(settings_file)
-        for key in settings:
-            settings[key] = data[key] if key in data else None
-except FileNotFoundError:
-    pass
+# try:
+#     import os
+#     this_dir = os.path.dirname(os.path.abspath(__file__))
+#     settings_file_name = os.path.join(this_dir, '..', 'settings.json')
+#     with open(settings_file_name) as settings_file:
+#         data = json.load(settings_file)
+#         for key in settings:
+#             settings[key] = data[key] if key in data else None
+# except FileNotFoundError:
+#     pass
 
 for key in settings:
     if key in os.environ:
         settings[key] = os.environ[key]
-    # if settings[key] is None:
-    #     raise EnvironmentError("%s not set (add to environment or settings.remote.json)" % key)
+    if settings[key] is None:
+        raise EnvironmentError("%s not set (add to environment)" % key)
 
 if 'DATA_FOLDER' in settings and settings['DATA_FOLDER'] is not None:
     if not os.path.exists(settings['DATA_FOLDER']):
