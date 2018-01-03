58617c78f415dcb259d9463be2a7cc1ac9b9c67b
Sietse Veenman <sietse@siloagency.nl>
2018-01-03 09:48:12 +0100

    Read more toggle script and css adjustments.

--- static/scripts/software_scripts.js
+++ static/scripts/software_scripts.js
@@ -1,5 +1,72 @@
-$(document).ready(function() {
+function collapseSection(element) {
+    // get the height of the element's inner content, regardless of its actual size
+    var sectionHeight = element.scrollHeight;
     
+    // temporarily disable all css transitions
+    var elementTransition = element.style.transition;
+    element.style.transition = '';
+    
+    // on the next frame (as soon as the previous style change has taken effect),
+    // explicitly set the element's height to its current pixel height, so we 
+    // aren't transitioning out of 'auto'
+    requestAnimationFrame(function() {
+      element.style.height = sectionHeight + 'px';
+      element.style.transition = elementTransition;
+      
+      // on the next frame (as soon as the previous style change has taken effect),
+      // have the element transition to height: 0
+      requestAnimationFrame(function() {
+        element.style.height = 0 + 'px';
+      });
+    });
+    
+    // mark the section as "currently collapsed"
+    element.setAttribute('data-collapsed', 'true');
+}
+  
+function expandSection(element) {
+    // get the height of the element's inner content, regardless of its actual size
+    var sectionHeight = element.scrollHeight;
+    
+    // have the element transition to the height of its inner content
+    element.style.height = sectionHeight + 'px';
+  
+    // when the next css transition finishes (which should be the one we just triggered)
+    element.addEventListener('transitionend', function handler(e) {
+      // remove this event listener so it only gets triggered once
+      element.removeEventListener('transitionend', handler);
+      
+      // remove "height" from the element's inline styles, so it can return to its initial value
+      element.style.height = null;
+    });
+    
+    // mark the section as "currently not collapsed"
+    element.setAttribute('data-collapsed', 'false');
+}
+
+
+
+
+$(document).ready(function() {
+
+    var moreContent = document.querySelector('.read-more_content');
+
+    $('.read-more_button').on("click", function() {
+
+        var buttonText = this.querySelector('.button_text');
+        
+        this.classList.toggle('active');
+
+        if ( !moreContent.getAttribute('data-collapsed') || moreContent.getAttribute('data-collapsed') === 'true' ) {
+            expandSection(moreContent);
+            
+            buttonText.textContent = 'Read less';
+        } else {
+            collapseSection(moreContent);
+            buttonText.textContent = 'Read more';
+        }
+    });
+   
 
     $(".mention_button").each(function() {
         $(this).on("click", function() {
