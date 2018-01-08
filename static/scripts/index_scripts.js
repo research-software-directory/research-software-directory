578653cc4cee8e27b3b3bb1a79b296643a75127f
Tom Klaver <tomklav@gmail.com>
2018-01-08 14:34:01 +0100

    limit software list based on window.innerWidth & add organization filter to software list

--- static/scripts/index_scripts.js
+++ static/scripts/index_scripts.js
@@ -9,7 +9,25 @@ var gaSearch = debounce(function(search) {
     }
 }, 3000);
 
-function initOverview(softwareData) {
+function initOverview(softwareData, organizationsData) {
+    function sortByKey(key) {
+        return function(a, b) { return a[key] - b[key]; }
+    }
+
+    function hasKey(key) {
+        return function(a) { return a.hasOwnProperty(key); }
+    }
+
+    var device = {
+        phone: 'phone',
+        tablet: 'tablet',
+        desktop: 'desktop'
+    };
+
+    function getDevice() {
+        return window.innerWidth > 1000 ? device.desktop : (window.innerWidth > 700 ? device.tablet : device.phone);
+    }
+
     var v = new Vue({
         el: '#overview',
         delimiters: ["[[", "]]"],
@@ -17,6 +35,9 @@ function initOverview(softwareData) {
             log: console.log,
             showPage: function (n) {
                 return n === 1 || n === this.lastPage || Math.abs(n - this.page) <= 2
+            },
+            getOrganizationById: function(id) {
+                return this.organizations.find(function(org) { return org.id === id; });
             }
         },
         data: {
@@ -36,12 +57,14 @@ function initOverview(softwareData) {
             ],
             filter: {
                 search: '',
-                tags: []
+                tags: [],
+                organizations: []
             },
             sort: 'Last updated',
-            pageSize: 10,
+            device: getDevice(),
             page: 1,
-            software: softwareData
+            software: softwareData,
+            organizations: organizationsData
         },
         computed: {
             tagCount: function () {
@@ -58,6 +81,22 @@ function initOverview(softwareData) {
                 return counts;
             },
 
+            organizationsWithCount: function () {
+                var counts = JSON.parse(JSON.stringify(this.organizations));
+                this.software.forEach(function (sw) {
+                    sw.contributingOrganization.forEach(function (orgId) {
+                        var org = counts.find(function(corg) { return corg.id === orgId });
+                        if (org) {
+                            org['count'] = (org['count'] || 0) + 1;
+                        }
+                    });
+                });
+
+                return counts.filter(hasKey('count')).sort(sortByKey('count')).reverse();
+            },
+
+
+
             filteredSoftware: function () {
                 function filterTags(tags) {
                     return function (sw) {
@@ -65,6 +104,19 @@ function initOverview(softwareData) {
                         var match = false;
                         sw.tags.forEach(function (tag) {
                             if (tags.includes(tag)) {
+                                match = true;
+                            }
+                        });
+                        return match;
+                    }
+                }
+
+                function filterOrganizations(orgs) {
+                    return function(sw) {
+                        if (orgs.length === 0) return true;
+                        var match = false;
+                        sw.contributingOrganization.forEach(function (org) {
+                            if (orgs.includes(org)) {
                                 match = true;
                             }
                         });
@@ -80,8 +132,11 @@ function initOverview(softwareData) {
                     }
                 }
 
+
+
                 return this.software
                     .filter(filterTags(this.filter.tags))
+                    .filter(filterOrganizations(this.filter.organizations))
                     .filter(filterSearch(this.filter.search));
             },
 
@@ -128,11 +183,21 @@ function initOverview(softwareData) {
                 this.software.map(function (sw) {
                     return "test";
                 });
+            },
+
+            pageSize: function() {
+                return {
+                    phone: 5,
+                    tablet: 10,
+                    desktop: 10
+                }[this.device];
             }
         },
 
         watch: {
             sort: {
+                handler: function () { this.page = 1; } },
+            pageSize: {
                 handler: function () { this.page = 1; } },
             filter: {
                 handler: function () { this.page = 1; },
@@ -143,4 +208,10 @@ function initOverview(softwareData) {
             }
         }
     });
+
+    window.addEventListener('resize', debounce(function(event) {
+        v.device = getDevice();
+    }, 200));
+
+
 }
