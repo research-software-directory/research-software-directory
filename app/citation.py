34eb7aca4f16ea82bb2b5fb9095ca5c890c65dee
Tom Klaver <tomklav@gmail.com>
2018-01-16 15:17:37 +0100

    udpate citation backend

---
+++ app/citation.py
@@ -0,0 +1,38 @@
+from citationcff import citation
+import requests
+
+def get_mime_and_extension(format):
+    if format == 'bibtex':
+        return 'application/x-bibtex', 'bib'
+    if format == 'enw':
+        return 'application/x-endnote-refer', 'enw'
+    if format == 'ris':
+        return 'application/x-research-info-systems', 'ris'
+    raise Exception('unknown format %s' % format)
+
+def get_citation_data_cff(c, format):
+    if format == 'bibtex':
+        return c.as_bibtex()
+    if format == 'enw':
+        return c.as_enw()
+    if format == 'ris':
+        return c.as_ris()
+
+def get_citation_data_citeas(github_id, format):
+    url = 'http://api.citeas.org/product/https://github.com/%s' % github_id
+    res = requests.get(url).json()
+    return [value for value in res['exports'] if value['export_name'] == format][0]['export']
+
+def get_citation(github_id, format):
+    mime, extension = get_mime_and_extension(format)
+    try:
+        c = citation.Citation('https://github.com/%s' % github_id)
+    except:
+        c = None
+
+    if isinstance(c, citation.Citation):
+        data = get_citation_data_cff(c, format)
+    else:
+        data = get_citation_data_citeas(github_id, format)
+
+    return mime, extension, data
