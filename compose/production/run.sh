ca3d34bdb7120f6f7ff223f0bd1c27895feb0a70
Tom Klaver <tomklav@gmail.com>
2018-01-18 17:42:31 +0100

    fix nginx config

--- compose/production/run.sh
+++ compose/production/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This file is the entrypoint for Docker, do not run on your local system
+rm /etc/nginx/sites-enabled/default
 if [[ -z "${DOMAIN}" ]] || [ ! -f /cert/live/$DOMAIN/privkey.pem ]; then
     echo "\$DOMAIN not set, or key not found at /cert/live/\$DOMAIN/, cannot setup HTTPS"
     cp compose/production/config/nginx.http.conf.template /etc/nginx/conf.d/default.conf
