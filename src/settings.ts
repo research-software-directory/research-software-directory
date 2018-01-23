7e2cc0a3fc96303a9dd01a7d1629409893b09a1f
Tom Klaver <tomklav@gmail.com>
2018-01-23 17:58:47 +0100

    fix: construct api location from current domain name

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 let BACKEND_URL = '';
 if (window && window.location && window.location.href) {
-  const s = window.location.href.replace(/https?:\/\//, '').split('.');
+  const s = window.location.hostname.split('.');
   // tslint:disable-next-line:no-http-string
   BACKEND_URL = process.env.REACT_APP_BACKEND || ('//' + 'api.' + s[s.length - 2] + '.' + s[s.length - 1]);
 }
