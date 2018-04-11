deac28bb5b4e81af93903493ae41738c7ef6af77
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-04-11 16:31:28 +0200

    Added delete to array form elements

--- src/components/Form/TypeArray.tsx
+++ src/components/Form/TypeArray.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 
 import { Button } from "semantic-ui-react";
+import styled from "styled-components";
+
 import { IArraySchema } from "../../interfaces/json-schema";
 import { IProps } from "./IProps";
 import FormPart from "./FormPart";
@@ -27,6 +29,13 @@ export default class TypeArray extends React.Component<IProps<IArraySchema>> {
     this.props.onChange([...value, createEmpty(this.props.schema.items)]);
   };
 
+  onDelete = (index: any) => {
+    const valCopy = this.props.value.filter(
+      (_element: any, i: number) => i !== index
+    );
+    this.props.onChange(valCopy);
+  };
+
   render() {
     let value = this.props.value;
     if (!Array.isArray(value)) {
@@ -37,17 +46,23 @@ export default class TypeArray extends React.Component<IProps<IArraySchema>> {
       <div>
         <Button onClick={this.onAdd}>+ Add</Button>
         {value.map((val: any, index: number) => (
-          <FormPart
-            key={index}
-            value={val}
-            settings={this.props.settings}
-            schema={this.props.schema.items}
-            data={this.props.data}
-            label=""
-            onChange={(v: any) => this.handleChange(index, v)}
-          />
+          <Section key={index}>
+            <FormPart
+              value={val}
+              settings={this.props.settings}
+              schema={this.props.schema.items}
+              data={this.props.data}
+              label=""
+              onChange={(v: any) => this.handleChange(index, v)}
+            />
+            <Button onClick={() => this.onDelete(index)}>x Delete</Button>
+          </Section>
         ))}
       </div>
     );
   }
 }
+
+const Section = styled.section`
+  border: 1px solid black;
+`;
