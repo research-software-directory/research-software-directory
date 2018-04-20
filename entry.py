513a5bab89e7591b7d141b615a6524e9ea99c3de
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-04-20 12:04:29 +0200

    Separate section for testimonials
    Refs #68

--- entry.py
+++ entry.py
@@ -1,10 +1,6 @@
 from app.application import application
-from sassutils.wsgi import SassMiddleware
 
 if __name__ == '__main__':
     application.jinja_env.auto_reload = True
     application.config['TEMPLATES_AUTO_RELOAD'] = True
-    application.wsgi_app = SassMiddleware(application.wsgi_app, {
-        __name__: ('style', 'static/style', '/static/style')
-    })
     application.run(debug=True, host='0.0.0.0', port=5004)
