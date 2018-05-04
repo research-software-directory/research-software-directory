ce1d751af398929b3a7b4474d458afdb0cac7fd4
Tom Klaver <tomklav@gmail.com>
2018-05-04 16:26:05 +0200

    fix tests

--- tests/test_software_page.py
+++ tests/test_software_page.py
@@ -12,7 +12,7 @@ def get():
     def _get(url):
         client = application.test_client()
         response = client.get(url)
-        return response.data.decode('utf-8')
+        return response.data.decode('utf-8'), response.status_code
     return _get
 
 
@@ -28,8 +28,9 @@ for path in glob.glob('mocks/software/*.json'):
 def test_fixtures_render(get, slug):
     with requests_mock.mock() as m:
         m.get(api_url + '/software_cache/%s' % slug, text=get_mock('software/%s.json' % slug))
-        result = get('/software/xenon')
-        assert isValidHTML(result)
+        data, status_code = get('/software/xenon')
+        assert status_code == 200
+        assert isValidHTML(data)
 
 live_software_items = []
 if pytest.config.getoption("live"):
@@ -40,59 +41,7 @@ if pytest.config.getoption("live"):
 @pytest.mark.skipif(not pytest.config.getoption("live"), reason="--live not specified")
 @pytest.mark.parametrize("slug", live_software_items)
 def test_live_software_data_renders(get, slug):
-    result = get('/software/xenon')
-    assert isValidHTML(result)
+    data, status_code = get('/software/%s' % slug)
+    assert status_code == 200
+    assert isValidHTML(data)
 
-
-
-
-
-
-# def test_live():
-#     print(software_items)
-#
-#     assert False
-
-# import json
-# import pytest
-# import glob
-# import os
-#
-# from jsonschema import validate, ValidationError
-#
-# pwd = os.path.dirname(__file__)
-#
-# schemas_folder = os.path.join(pwd, '..', '..', 'schemas')
-# fixtures_folder = os.path.join(pwd, '..', '..', 'schema-fixtures')
-#
-# schemas = {}
-#
-# for (path, file) in [(path, open(path)) for path in glob.glob('%s/*.json' % schemas_folder)]:
-#     file_name = path.split('/')[-1]
-#     schema_name = file_name.split('.')[0]
-#     schemas[schema_name] = json.load(file)
-#
-# def test_schemas_exist():
-#     assert schemas is not None
-#
-#
-# def test_at_least_three_schemas_exist():
-#     assert len(schemas) > 3
-#
-#
-# parameters = [
-#     (schema_name, fixture_path) for
-#     schema_name in schemas.keys() for
-#     fixture_path in glob.glob('%s/%s*.json' % (fixtures_folder, schema_name))
-# ]
-#
-#
-# @pytest.mark.parametrize("schema_name, fixture_path", parameters)
-# def test_fixtures_are_valid(schema_name, fixture_path):
-#     error = None
-#     try:
-#         validate(json.load(open(fixture_path)), schemas[schema_name])
-#     except ValidationError as e:
-#         error = e.message + ": " + fixture_path + \
-#                 ' @ ' + ' -> '.join(str(path_part) for path_part in list(e.relative_path))
-#     assert error is None
