bfbf2c986af0379cb89eb65eb2a755f768ec7dd5
Tom Klaver <tomklav@gmail.com>
2018-05-25 14:50:29 +0200

    add Delete button for resources

--- src/components/Resource/index.tsx
+++ src/components/Resource/index.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import axios from "axios";
 
 import { RouteComponentProps } from "react-router";
-import { Button, Message } from "semantic-ui-react";
+import { Button, Message, Modal, Header, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 import { IJWT, ISettings } from "../../rootReducer";
 import { ISchema } from "../../interfaces/json-schema";
@@ -20,15 +21,110 @@ interface IConnectedProps {
   errorToastr(message: string): any;
 }
 
+type IProps = IConnectedProps &
+  RouteComponentProps<{ resourceType: string; id: string }>;
+
+class DeleteModal extends React.PureComponent<
+  IProps,
+  { modalOpen: boolean; links: any[]; error: string | null }
+> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = { modalOpen: false, links: [], error: null };
+  }
+
+  confirmDelete() {
+    const { resourceType, id } = this.props.match.params;
+    const { backendUrl } = this.props.settings;
+
+    axios
+      .delete(`${backendUrl}/${resourceType}/${id}`, {
+        headers: {
+          Authorization: `Bearer ${this.props.jwt.token}`
+        }
+      })
+      .then(() => {
+        this.setState({ modalOpen: false, links: [], error: null });
+        this.props.messageToastr("Deleted");
+      })
+      .catch(response => {
+        const result = response.response;
+        if (result && result.status === 406) {
+          this.setState({ links: result.data.data, error: result.data.error });
+        } else {
+          this.setState({ links: [], error: result.data.error });
+        }
+      });
+  }
+
+  formatLink(linkArr: any[]): any {
+    const linkParts = [...linkArr];
+    const resourceType = linkParts.shift();
+    const id = linkParts.shift();
+    return (
+      <div>
+        <Link to={`/${resourceType}/${id}`}>{`/${resourceType}/${id}`}</Link>
+        {linkParts
+          .map(part => (typeof part === "string" ? ` => ${part}` : `[${part}]`))
+          .join("")}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Modal
+        trigger={
+          <Button
+            color="red"
+            onClick={() => this.setState({ modalOpen: true })}
+          >
+            Delete
+          </Button>
+        }
+        open={this.state.modalOpen}
+        onClose={() => this.setState({ modalOpen: false })}
+        size="small"
+        style={{
+          marginTop: "0px !important",
+          marginLeft: "auto",
+          marginRight: "auto"
+        }}
+      >
+        <Header icon="delete" content="Deleting" />
+        <Modal.Content>
+          <p>Are you sure you want to delete this?</p>
+          {this.state.error && (
+            <Message
+              error={true}
+              header={this.state.error}
+              list={this.state.links.map(this.formatLink)}
+            />
+          )}
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="red" onClick={() => this.confirmDelete()}>
+            <Icon name="remove" /> Yes, delete
+          </Button>
+          <Button
+            onClick={() => this.setState({ modalOpen: false })}
+            inverted={true}
+            color="green"
+          >
+            <Icon name="checkmark" /> No
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
+  }
+}
+
 interface IState {
   loading: boolean;
   saving: boolean;
   data: IResource | null;
   validationErrors: any[];
 }
-
-type IProps = IConnectedProps &
-  RouteComponentProps<{ resourceType: string; id: string }>;
 
 export default class Resource extends React.PureComponent<IProps, IState> {
   form: any = null;
@@ -123,11 +219,13 @@ export default class Resource extends React.PureComponent<IProps, IState> {
           </Message>
         )}
         <Button
+          primary={true}
           disabled={this.state.saving || this.state.validationErrors.length > 0}
           onClick={this.save}
         >
           {this.state.saving ? "Saving..." : "Save"}
         </Button>
+        <DeleteModal {...this.props} />
       </div>
     );
   }
