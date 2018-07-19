a75a8139509c9f85ffd448041b3a2acac90267eb
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2018-07-19 12:45:42 +0200

    sorted partner institutions for display in the filter sidebar on the index page

--- static/scripts/index_scripts.js
+++ static/scripts/index_scripts.js
@@ -162,6 +162,12 @@ function initOverview(softwareData, organizationsData) {
 
             /* organizations for which there is at least one software item with this org as contributingOrganization */
             partnerOrganizations: function() {
+
+                var alphabeticallyByName = function (obj1, obj2) {
+                    var locale = undefined;
+                    var options = {sensitivity: 'base'};  // case-insensitive
+                    return obj1.name.localeCompare(obj2.name, locale, options);
+                }
                 // all contributing organization ids (map, flatten, filter unique)
                 var contributingOrganizationIds = [].concat.apply(
                     [],
@@ -169,11 +175,12 @@ function initOverview(softwareData, organizationsData) {
                 )
                 .map(function(org) { return org.foreignKey.primaryKey.id; })
                 .filter(filterUnique);
-
+                // create a deep copy of the array of organizations
                 var orgCopy = JSON.parse(JSON.stringify(this.organizations));
-                return orgCopy.filter(
-                    function(org) { return contributingOrganizationIds.indexOf(org.primaryKey.id) !== -1 }
-                );
+                return orgCopy.filter(function(org) {
+                    return contributingOrganizationIds.indexOf(org.primaryKey.id) !== -1 
+                })
+                .sort(alphabeticallyByName);
             },
 
             organizationsWithCount: function () {
