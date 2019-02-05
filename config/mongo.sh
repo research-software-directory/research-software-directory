3bee8ac751bc49c3c3c7d9a102c1f1e7584f3560
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-02-05 12:08:51 +0100

    all docker volumes are now created as subdirs in one directory; renamed initial database contents to db-init to indicate the direction of information

--- config/mongo.sh
+++ config/mongo.sh
@@ -6,8 +6,8 @@ MONGOD_PID=$!
 sleep 5
 
 if mongo --quiet rsd --eval "db.getCollectionNames().length" | egrep '^0\s?$' ; then
-    echo "Mongo DB seems empty, populating from db-dump"
-    mongorestore -d rsd /db-dump-initial
+    echo "Mongo DB seems empty, populating from db-init"
+    mongorestore -d rsd /db-init
 fi
 
 wait $MONGOD_PID
