91fd06a306b01378c5dbbc7b3c5f3825d4449e56
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-07-01 11:02:09 +0200

    Dont fill db, use admin interface to create entries

--- database/mongo.sh
+++ database/mongo.sh
@@ -5,9 +5,9 @@ MONGOD_PID=$!
 
 sleep 5
 
-if mongo --quiet rsd --eval "db.getCollectionNames().length" | egrep '^0\s?$' ; then
-    echo "Mongo DB seems empty, populating from db-init"
-    mongorestore -d rsd /db-init
-fi
+# if mongo --quiet rsd --eval "db.getCollectionNames().length" | egrep '^0\s?$' ; then
+#     echo "Mongo DB seems empty, populating from db-init"
+#     mongorestore -d rsd /db-init
+# fi
 
 wait $MONGOD_PID
