8544ba5820b5b9ebfa5ea2b6abad86116d7f9fb1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2019-10-02 13:01:51 +0200

    removed unused code from /graphs/

--- graphs/random-old-stuff/brand/projects.js
+++
@@ -1,7 +0,0 @@
-let get_projects = (product_page) => {
-    return product_page.related.projects;
-}
-
-let get_number_of_projects = (product_page) => {
-    return product_page.related.projects.length;
-}
