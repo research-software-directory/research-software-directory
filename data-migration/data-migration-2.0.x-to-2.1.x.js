98e59e199ca0de5e02e1e5c193c8d0b1680708ef
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-10-21 12:27:39 +0200

    updated data migration script (omits non-required keys; unifies format of project img urls)

--- data-migration/data-migration-2.0.x-to-2.1.x.js
+++ data-migration/data-migration-2.0.x-to-2.1.x.js
@@ -1,5 +1,9 @@
 var query = {}
 
+// Note: keys 'dataManagementPlanUrl', 'homeUrl', 'imageCaption', and
+// 'softwareSustainabilityPlanUrl' are purposely not set, as they are
+// not required. Users can still set them through the admin
+// interface if they want though.
 var update = {
     "$set": {
         "callUrl": "https://doi.org/FIXME",
@@ -8,9 +12,7 @@ var update = {
         "dateStart": "1900-01-01T00:00:00Z",
         "description": "FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME",
         "grantId": "FIXME",
-        "homeUrl": "https://fixme.org",
-        "imageCaption": "this is the image caption",
-        "isPublished": false,
+        "isPublished": true,
         "related.organizations": [],
         "related.projects": [],
         "related.software": [],
@@ -27,3 +29,26 @@ var update = {
 }
 
 db.project.updateMany(query, update)
+
+
+// Make links to corporate site uniform
+// - only https links
+// - no www subdomain to esciencecenter.nl
+
+db.project.find({
+    "imageUrl": {
+        $regex: "^http://www.esciencecenter.nl"
+    }
+}).forEach((doc, idoc) => {
+    doc.imageUrl = doc.imageUrl.replace("http://www.esciencecenter.nl", "https://esciencecenter.nl")
+    db.project.save(doc)
+})
+
+db.project.find({
+    "imageUrl": {
+        $regex: "^https://www.esciencecenter.nl"
+    }
+}).forEach((doc, idoc) => {
+    doc.imageUrl = doc.imageUrl.replace("https://www.esciencecenter.nl", "https://esciencecenter.nl")
+    db.project.save(doc)
+})
