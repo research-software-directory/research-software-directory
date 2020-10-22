33d2ca8eaa3df0c192dbd5258a30fce1b79e23e9
Erik Tjong Kim Sang <erikt@xs4all.nl>
2020-10-22 15:47:49 +0200

    adapted tests to rate limit check changes

--- harvesting/util_unit_test.py
+++ harvesting/util_unit_test.py
@@ -3,17 +3,17 @@ import requests
 from releases import rate_limit_reached
 
 
-class rateLimitReachedTest(unittest.TestCase):
+class RateLimitReachedTest(unittest.TestCase):
 
     def test_rate_limit_reached_none_input(self):
         actual_return = rate_limit_reached(None)
-        expected_return = True
+        expected_return = False
         self.assertEqual(expected_return, actual_return)
 
     def test_rate_limit_reached_irrelevant_url_input(self):
         r = requests.get("https://esciencecenter.nl", headers={})
         actual_return = rate_limit_reached(r)
-        expected_return = True
+        expected_return = False
         self.assertEqual(expected_return, actual_return)
 
 
