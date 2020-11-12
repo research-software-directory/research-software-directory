8fc9b63b80dd6fcd56e5a009b18269444f33b086
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-11-12 15:13:02 +0100

    Fix lint errors

--- data-migration/data-migration-project-date.js
+++ data-migration/data-migration-project-date.js
@@ -1,23 +1,25 @@
+/* global db */
+
 // Upgrade project datetime fields to dates
 // Copy paste content of file in mongo shell with `docker-compose exec database mongo rsd`
 
 const query = {
-    dateStart: '1900-01-01T00:00:00Z'
+  dateStart: '1900-01-01T00:00:00Z'
 }
 const update = {
-    $set: {
-      dateEnd: '1901-01-01',
-      dateStart: '1900-01-01'
-    },
+  $set: {
+    dateEnd: '1901-01-01',
+    dateStart: '1900-01-01'
   }
+}
 
-db.project.updateMany(query, update);
+db.project.updateMany(query, update)
 
 db.project.update({
-    slug: 'abc-muse'
+  slug: 'abc-muse'
 }, {
-    $set: {
-        dateEnd: '2021-08-19',
-        dateStart: '2018-08-19',
-    }
-});
+  $set: {
+    dateEnd: '2021-08-19',
+    dateStart: '2018-08-19'
+  }
+})
