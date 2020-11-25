86d262beb7f604d511d648d40fad80c0ce6efcf6
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-11-25 14:33:33 +0100

    fixed linter problems

--- data-migration/2.x-to-3.x/migrate.js
+++ data-migration/2.x-to-3.x/migrate.js
@@ -1,91 +1,87 @@
 /* global db */
 
-const migrate_project_collection = () => {
+const migrateProjectCollection = () => {
+  print('migrating project collection...')
 
-    print("migrating project collection...")
+  const query = {}
 
-    const query = {}
-
-    // Note: keys 'dataManagementPlanUrl', 'homeUrl', 'imageCaption', and
-    // 'softwareSustainabilityPlanUrl' are purposely not set, as they are
-    // not required. Users can still set them through the admin
-    // interface if they want though.
-    const update = {
-      $set: {
-        callUrl: 'https://doi.org/FIXME',
-        codeUrl: 'https://github.com/FIXME',
-        dateEnd: '1901-01-01',
-        dateStart: '1900-01-01',
-        description: 'FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME',
-        grantId: 'FIXME',
-        isPublished: true,
-        'related.organizations': [],
-        'related.projects': [],
-        'related.software': [],
-        tags: [],
-        team: []
-      },
-      $rename: {
-        image: 'imageUrl'
-      },
-      $unset: {
-        corporateUrl: '',
-        principalInvestigator: ''
-      }
+  // Note: keys 'dataManagementPlanUrl', 'homeUrl', 'imageCaption', and
+  // 'softwareSustainabilityPlanUrl' are purposely not set, as they are
+  // not required. Users can still set them through the admin
+  // interface if they want though.
+  const update = {
+    $set: {
+      callUrl: 'https://doi.org/FIXME',
+      codeUrl: 'https://github.com/FIXME',
+      dateEnd: '1901-01-01',
+      dateStart: '1900-01-01',
+      description: 'FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME',
+      grantId: 'FIXME',
+      isPublished: true,
+      'related.organizations': [],
+      'related.projects': [],
+      'related.software': [],
+      tags: [],
+      team: []
+    },
+    $rename: {
+      image: 'imageUrl'
+    },
+    $unset: {
+      corporateUrl: '',
+      principalInvestigator: ''
     }
+  }
 
-    db.project.updateMany(query, update)
+  db.project.updateMany(query, update)
 
-    print("migrating project collection...done")
-
+  print('migrating project collection...done')
 }
 
-const migrate_organization_collection = () => {
+const migrateOrganizationCollection = () => {
+  print('migrating organization collection...')
 
-    print("migrating organization collection...")
+  const query = {}
 
-    const query = {}
-
-    const update = {
-      $unset: {
-        url: ''
-      }
+  const update = {
+    $unset: {
+      url: ''
     }
+  }
 
-    db.organization.updateMany(query, update)
+  db.organization.updateMany(query, update)
 
-    print("migrating organization collection...done")
+  print('migrating organization collection...done')
 }
 
+const harmonizeProjectUrls = () => {
+  // Make links to corporate site uniform
+  // - only https links
+  // - no www subdomain to esciencecenter.nl
 
-const harmonize_project_urls = () => {
-    // Make links to corporate site uniform
-    // - only https links
-    // - no www subdomain to esciencecenter.nl
+  print('harmonizing project urls...')
 
-    print("harmonizing project urls...")
+  db.project.find({
+    imageUrl: {
+      $regex: '^http://www.esciencecenter.nl'
+    }
+  }).forEach((doc, idoc) => {
+    doc.imageUrl = doc.imageUrl.replace('http://www.esciencecenter.nl', 'https://esciencecenter.nl')
+    db.project.save(doc)
+  })
 
-    db.project.find({
-      imageUrl: {
-        $regex: '^http://www.esciencecenter.nl'
-      }
-    }).forEach((doc, idoc) => {
-      doc.imageUrl = doc.imageUrl.replace('http://www.esciencecenter.nl', 'https://esciencecenter.nl')
-      db.project.save(doc)
-    })
+  db.project.find({
+    imageUrl: {
+      $regex: '^https://www.esciencecenter.nl'
+    }
+  }).forEach((doc, idoc) => {
+    doc.imageUrl = doc.imageUrl.replace('https://www.esciencecenter.nl', 'https://esciencecenter.nl')
+    db.project.save(doc)
+  })
 
-    db.project.find({
-      imageUrl: {
-        $regex: '^https://www.esciencecenter.nl'
-      }
-    }).forEach((doc, idoc) => {
-      doc.imageUrl = doc.imageUrl.replace('https://www.esciencecenter.nl', 'https://esciencecenter.nl')
-      db.project.save(doc)
-    })
-
-    print("harmonizing project urls...done")
+  print('harmonizing project urls...done')
 }
 
-migrate_project_collection()
-harmonize_project_urls()
-migrate_organization_collection()
+migrateProjectCollection()
+harmonizeProjectUrls()
+migrateOrganizationCollection()
