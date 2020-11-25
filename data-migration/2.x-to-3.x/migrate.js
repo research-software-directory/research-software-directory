07ee22959f66fe6a606c626b1def621ebd5f8612
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-11-25 14:00:58 +0100

    updated migration script so it removes unused property url; scoped the migration script with functions; added print statements for better feedback

--- data-migration/2.x-to-3.x/migrate.js
+++ data-migration/2.x-to-3.x/migrate.js
@@ -1,55 +1,91 @@
 /* global db */
 
-const query = {}
+const migrate_project_collection = () => {
 
-// Note: keys 'dataManagementPlanUrl', 'homeUrl', 'imageCaption', and
-// 'softwareSustainabilityPlanUrl' are purposely not set, as they are
-// not required. Users can still set them through the admin
-// interface if they want though.
-const update = {
-  $set: {
-    callUrl: 'https://doi.org/FIXME',
-    codeUrl: 'https://github.com/FIXME',
-    dateEnd: '1901-01-01',
-    dateStart: '1900-01-01',
-    description: 'FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME',
-    grantId: 'FIXME',
-    isPublished: true,
-    'related.organizations': [],
-    'related.projects': [],
-    'related.software': [],
-    tags: [],
-    team: []
-  },
-  $rename: {
-    image: 'imageUrl'
-  },
-  $unset: {
-    corporateUrl: '',
-    principalInvestigator: ''
-  }
+    print("migrating project collection...")
+
+    const query = {}
+
+    // Note: keys 'dataManagementPlanUrl', 'homeUrl', 'imageCaption', and
+    // 'softwareSustainabilityPlanUrl' are purposely not set, as they are
+    // not required. Users can still set them through the admin
+    // interface if they want though.
+    const update = {
+      $set: {
+        callUrl: 'https://doi.org/FIXME',
+        codeUrl: 'https://github.com/FIXME',
+        dateEnd: '1901-01-01',
+        dateStart: '1900-01-01',
+        description: 'FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME FIXME',
+        grantId: 'FIXME',
+        isPublished: true,
+        'related.organizations': [],
+        'related.projects': [],
+        'related.software': [],
+        tags: [],
+        team: []
+      },
+      $rename: {
+        image: 'imageUrl'
+      },
+      $unset: {
+        corporateUrl: '',
+        principalInvestigator: ''
+      }
+    }
+
+    db.project.updateMany(query, update)
+
+    print("migrating project collection...done")
+
 }
 
-db.project.updateMany(query, update)
+const migrate_organization_collection = () => {
 
-// Make links to corporate site uniform
-// - only https links
-// - no www subdomain to esciencecenter.nl
+    print("migrating organization collection...")
 
-db.project.find({
-  imageUrl: {
-    $regex: '^http://www.esciencecenter.nl'
-  }
-}).forEach((doc, idoc) => {
-  doc.imageUrl = doc.imageUrl.replace('http://www.esciencecenter.nl', 'https://esciencecenter.nl')
-  db.project.save(doc)
-})
+    const query = {}
 
-db.project.find({
-  imageUrl: {
-    $regex: '^https://www.esciencecenter.nl'
-  }
-}).forEach((doc, idoc) => {
-  doc.imageUrl = doc.imageUrl.replace('https://www.esciencecenter.nl', 'https://esciencecenter.nl')
-  db.project.save(doc)
-})
+    const update = {
+      $unset: {
+        url: ''
+      }
+    }
+
+    db.organization.updateMany(query, update)
+
+    print("migrating organization collection...done")
+}
+
+
+const harmonize_project_urls = () => {
+    // Make links to corporate site uniform
+    // - only https links
+    // - no www subdomain to esciencecenter.nl
+
+    print("harmonizing project urls...")
+
+    db.project.find({
+      imageUrl: {
+        $regex: '^http://www.esciencecenter.nl'
+      }
+    }).forEach((doc, idoc) => {
+      doc.imageUrl = doc.imageUrl.replace('http://www.esciencecenter.nl', 'https://esciencecenter.nl')
+      db.project.save(doc)
+    })
+
+    db.project.find({
+      imageUrl: {
+        $regex: '^https://www.esciencecenter.nl'
+      }
+    }).forEach((doc, idoc) => {
+      doc.imageUrl = doc.imageUrl.replace('https://www.esciencecenter.nl', 'https://esciencecenter.nl')
+      db.project.save(doc)
+    })
+
+    print("harmonizing project urls...done")
+}
+
+migrate_project_collection()
+harmonize_project_urls()
+migrate_organization_collection()
