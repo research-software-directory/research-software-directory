97738c9e2679b617ef670a166361f36d08c532c9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-11-26 16:21:39 +0100

    frontend flask application now injects project_index template with image instead of imageUrl

--- frontend/app/application.py
+++ frontend/app/application.py
@@ -205,7 +205,7 @@ def project_index_template():
         projects.append({"id": project["primaryKey"]["id"],
                          "title": project["title"],
                          "subtitle": project["subtitle"],
-                         "imageUrl": project["imageUrl"],
+                         "image": project["image"],
                          "yearStart": get_year_from_date_string(project["dateStart"]),
                          "yearEnd": get_year_from_date_string(project["dateEnd"])})
     mentions = get_project_mentions(project_data)
