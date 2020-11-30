45fbe7fa05949dcad77c63085ec4a73502469678
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-11-30 18:51:40 +0100

    import renamed

--- data-migration/2.0-to-3.0/harvest_project_info_nlesc_unit_test.py
+++ data-migration/2.0-to-3.0/harvest_project_info_nlesc_unit_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-import harvest_project_info_nlesc as esciencecenter
+import harvest_project_info_nlesc
 import requests
 from zotero import generate_jwt_token
 
@@ -10,47 +10,47 @@ from zotero import generate_jwt_token
 class eScienceCenterTest(unittest.TestCase):
     def test_request_was_successful_success(self):
         response = requests.get("https://www.esciencecenter.nl/projects")
-        actual_value = esciencecenter.request_was_successful(response)
+        actual_value = harvest_project_info_nlesc.request_was_successful(response)
         self.assertTrue(actual_value)
 
     def test_request_was_successful_failure(self):
         response = requests.get(
             "https://www.esciencecenter.nl/projects/unknown-project.html"
         )
-        actual_value = esciencecenter.request_was_successful(response)
+        actual_value = harvest_project_info_nlesc.request_was_successful(response)
         self.assertFalse(actual_value)
 
     def test_is_project_url_success(self):
-        actual_value = esciencecenter.is_project_url(
+        actual_value = harvest_project_info_nlesc.is_project_url(
             "https://www.esciencecenter.nl/projects/project.html"
         )
         self.assertTrue(actual_value)
 
     def test_is_project_url_fails(self):
-        actual_value = esciencecenter.is_project_url(
+        actual_value = harvest_project_info_nlesc.is_project_url(
             "https://www.esciencecenter.nl/projects/"
         )
         self.assertFalse(actual_value)
 
     def test_remove_titles(self):
-        actual_value = esciencecenter.remove_titles(
+        actual_value = harvest_project_info_nlesc.remove_titles(
             "Prof.Mr.Dr.Drs.Ir. John Smith, PhD MSc BSc MA"
         )
         expected_value = "John Smith"
         self.assertEqual(expected_value, actual_value)
 
     def test_remove_tags(self):
-        actual_value = esciencecenter.remove_tags('<a href="url">text</a>')
+        actual_value = harvest_project_info_nlesc.remove_tags('<a href="url">text</a>')
         expected_value = "text"
         self.assertEqual(expected_value, actual_value)
 
     def test_replace_paragraph_tags(self):
-        actual_value = esciencecenter.replace_paragraph_tags("<p>text</p>")
+        actual_value = harvest_project_info_nlesc.replace_paragraph_tags("<p>text</p>")
         expected_value = "\n\ntext\n\n"
         self.assertEqual(expected_value, actual_value)
 
     def test_make_person_name_basic(self):
-        actual_value = esciencecenter.make_person_name(
+        actual_value = harvest_project_info_nlesc.make_person_name(
             {
                 "givenNames": "John",
                 "familyNames": "Smith",
@@ -62,7 +62,7 @@ class eScienceCenterTest(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
 
     def test_make_person_name_complex(self):
-        actual_value = esciencecenter.make_person_name(
+        actual_value = harvest_project_info_nlesc.make_person_name(
             {
                 "givenNames": "Jan",
                 "familyNames": "Jong",
@@ -74,27 +74,27 @@ class eScienceCenterTest(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
 
     def test_split_name_basic(self):
-        actual_value = esciencecenter.split_name("John Smith")
+        actual_value = harvest_project_info_nlesc.split_name("John Smith")
         expected_value = ("John", "", "Smith")
         self.assertEqual(expected_value, actual_value)
 
     def test_split_name_complex(self):
-        actual_value = esciencecenter.split_name("Jan de Jong jr.")
+        actual_value = harvest_project_info_nlesc.split_name("Jan de Jong jr.")
         expected_value = ("Jan", "de", "Jong jr.")
         self.assertEqual(expected_value, actual_value)
 
     def test_get_suffix(self):
-        actual_value = esciencecenter.get_suffix("file.png")
+        actual_value = harvest_project_info_nlesc.get_suffix("file.png")
         expected_value = "png"
         self.assertEqual(expected_value, actual_value)
 
     def test_fix_role(self):
-        actual_value = esciencecenter.fix_role("COMMUNITY MANAGER & TECHNICAL LEAD")
+        actual_value = harvest_project_info_nlesc.fix_role("COMMUNITY MANAGER & TECHNICAL LEAD")
         expected_value = "Community Manager"
         self.assertEqual(expected_value, actual_value)
 
     def test_guess_affiliations_engineer(self):
-        actual_value = esciencecenter.guess_affiliation(
+        actual_value = harvest_project_info_nlesc.guess_affiliation(
             {"role": "eScience Research Engineer"}, []
         )
         expected_value = {
@@ -106,7 +106,7 @@ class eScienceCenterTest(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
 
     def test_guess_affiliations_pi(self):
-        actual_value = esciencecenter.guess_affiliation(
+        actual_value = harvest_project_info_nlesc.guess_affiliation(
             {"role": "Principal investigator"}, ["University"]
         )
         expected_value = {
@@ -116,7 +116,7 @@ class eScienceCenterTest(unittest.TestCase):
         self.assertEqual(expected_value, actual_value)
 
     def test_make_url_secure(self):
-        actual_value = esciencecenter.make_url_secure("http://fix.me")
+        actual_value = harvest_project_info_nlesc.make_url_secure("http://fix.me")
         expected_value = "https://fix.me"
         self.assertEqual(expected_value, actual_value)
 
@@ -185,7 +185,7 @@ class eScienceCenterTest(unittest.TestCase):
 
     def test_person_name_checks(self):
         token = generate_jwt_token()
-        api_persons_data = esciencecenter.get_data_from_rsd(token, "/person")
+        api_persons_data = harvest_project_info_nlesc.get_data_from_rsd(token, "/person")
         self.check_duplicate_names(api_persons_data)
         self.check_extra_whitespace(api_persons_data)
         self.assertTrue(True)
